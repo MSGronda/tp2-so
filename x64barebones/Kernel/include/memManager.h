e6d2408c96408dfdf790f95bf4d19ba5fa0dd6c0
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-09-24 10:46:04 -0300

    feat: added memory manager. not tested

---
+++ x64barebones/Kernel/include/memManager.h
@@ -0,0 +1,12 @@
+#ifndef _MEMMANAGER_H_
+#define _MEMMANAGER_H_
+
+#include <stdlib.h>
+
+typedef struct MemoryManagerCDT *MemoryManagerADT;
+
+MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
+
+void *allocMemory(MemoryManagerADT const restrict memoryManager, const size_t memoryToAllocate);
+
+#endif
