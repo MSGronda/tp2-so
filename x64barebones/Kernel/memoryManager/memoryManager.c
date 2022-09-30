bf9f1d6cc0889815c6f49cd9102b5aad3763d3dc
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-09-30 20:04:34 -0300

    feat: added memory manager
    - Added implicit list memory manager, based on https://www.youtube.com/watch?v=74s0m4YoHgM
    - TODO: check if we can merge prev block in free

--- x64barebones/Kernel/memoryManager/memoryManager.c
+++ x64barebones/Kernel/memoryManager/memoryManager.c
@@ -1,112 +1,71 @@
 #include "memoryManager.h"
+#include <stdlib.h>
 
-typedef struct MemoryManagerCDT {
-	char *currentAddress;
-	memBlock * head;
-} MemoryManagerCDT;
+#define TRUE 1
+#define FALSE (!TRUE)
 
-typedef struct memBlock{
-	int size;	
-	int available; 		 	
-	char * address;
-	memBlock * next;
-	memBlock * prev;
-}memBlock;
+#define HEAP_SIZE 4096
 
-MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict firstAddress) {
-	MemoryManagerADT memoryManager = (MemoryManagerADT) memoryForMemoryManager;
-	memoryManager->currentAddress = firstAddress;
-	memoryManager->head = NULL;
+#define MASK_LAST_BIT(num) (((num) >> 1) << 1)
+#define GET_SIZE(ptr) (*(ptr) & -2)
+#define IS_ALLOCATED(ptr) (*(ptr) & 1)
+#define SET_ALLOCATED(size) ((size) | 1)
 
-	return memoryManager;
+static void * heapStart = 5000;
+static void * heapEnd = heapStart + HEAP_SIZE;
+
+
+// TODO: CHEQUEO DE ERRORES
+
+// TODO: HACER EL FREE PARA ATRAS TAMBIEN
+void freeBlock(void * ptr) {
+    *ptr = GET_SIZE(ptr);
+    void * next = ptr + *ptr;
+
+    if(IS_ALLOCATED(next) == FALSE)
+        *ptr = *ptr + *next;
 }
 
-void *allocMemory(MemoryManagerADT const restrict memoryManager, const size_t memoryToAllocate) {
-	
-	memBlock * currBlock = memoryManager->head;
 
-	//Itero hasta que el bloque:
-	//	- Este libre
-	//	- Y el tamano sea mayor a lo que quiero alocar
-	// 	- O no haya ningun bloque disponible.
-	while(currBlock != NULL && currBlock->available == 0 && currBlock->size < memoryToAllocate){
-		currBlock = currBlock-> next;
-	}
+// Allocate in a free block, split if needed
+void addBlock(void * ptr, uint64_t len) {
+    int64_t newSize = MASK_LAST_BIT(len + 1);
+    int64_t oldSize = GET_SIZE(ptr);
 
-	//Address a retornar
-	char * address;
-
-	//Si no hay bloque disponible, armo uno nuevo en la ultima direccion del MM
-	if(currBlock == NULL){
-
-		memBlock * newBlock = memoryManager->currentAddress;
-		newBlock -> size = memoryToAllocate;
-		newBlock -> available = 0;
-		newBlock -> address = newBlock; 
-
-		memBlock* iter = memoryManager->head;
-	
-		memoryManager->head = addBlock(memoryManager->head, newBlock);
-		
-
-		//Retorno el address + sizeof(memBlock) que le robe
-		address = memoryManager->currentAddress + sizeof(memBlock);
-		memoryManager->currentAddress += memoryToAllocate;
-
-	} else{
-		//Tengo un bloque disponible para asignar
-		currBlock->available = 0;
-		address = (currBlock->address + sizeof(memBlock));
-	}
-
-	return (void *) address;
-
+    *ptr = SET_HEADER(len);
+    if(newSize < oldSize)
+        *(ptr + newSize) = oldSize - newSize; 
 }
 
-memBlock * addBlock(memBlock * head, memBlock * toAdd){
+void * findFree(uint64_t len) {
+    void * ptr = heapStart;
 
-	if(head != NULL){
-		return toAdd;
-	} else if(head->data >= toAdd->data){
-		toAdd->next = head_ref;
-		toAdd->prev = NULL;
-		return toAdd;
-	} else{
-		memBlock * current = head;
-		while(current->next != NULL && current->next->data < toAdd->data)
-			current = current->next;
-		
-		toAdd->next = current->next;
-		if(current->next != NULL){
-			toAdd->next->prev = toAdd;
-			current->next = toAdd;
-		}
-		toAdd->prev = toAdd;
-	}
+    while( (ptr + len < heapEnd) && (IS_ALLOCATED(ptr) || (*ptr <= len) )
+        ptr = ptr + GET_SIZE(ptr);
 
-
+    if(ptr + len >= heapEnd)
+        return NULL;
+    return ptr;
 }
 
-void freeMemory(MemoryManagerADT const restrict memoryManager, void *memoryToFree) {
-	//TODO: Chequeo de error que el bloque no este
 
-	//El address que tiene el user esta adelantado 1 memBlock
+void * malloc(uint64_t len) {
+    void * out = findFree(len + sizeof(uint64_t));
+    addBlock(out, len + sizeof(uint64_t));
 
-	if(memoryToFree == NULL{
-		//EXPLOTAR TODO
-	}
-
-	char * address = memoryToFree - sizeof(memBlock);
-
-	memBlock * currBlock = memoryManager->head;
-	while(currBlock->address != address){
-		currBlock = currBlock->next;
-	}
-
-	if(currBlock == NULL){
-		//EXPLOTAR TODO
-		//NO se encontro el bloque que buscaba
-	}
-
-	currBlock->available = 1;
+    return out + sizeof(uint64_t);
 }
+
+
+void free(void * ptr) {
+    if(ptr == NULL || ptr < heapStart || ptr >= heapEnd)
+        return;
+    
+    freeBlock(ptr - sizeof(uint64_t));
+    *ptr = MASK_LAST_BIT(*ptr);
+}
+
+
+
+
+
