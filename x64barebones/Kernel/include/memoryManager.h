9da424889b309af1544cb7622527c9ba0cc07679
Manuel Dithurbide <mdithurbide@itba.edu.ar>
2022-10-02 20:08:53 -0300

    feat: mm compiles without warnings but has bugs

--- x64barebones/Kernel/include/memoryManager.h
+++ x64barebones/Kernel/include/memoryManager.h
@@ -1,19 +1,45 @@
 #ifndef _MEM_MANAGER_H_
 #define _MEM_MANAGER_H_
 
+#include <stdint.h>
 #include <stdlib.h>
 
-#define HEAP_SIZE (0xFFFFFFFFFFFFFFFF - 0x0000000000100000)
+typedef size_t header_t;
 
-typedef struct MemoryManagerCDT *MemoryManagerADT;
+#define HEADER_SIZE sizeof(header_t)
+#define TRUE 1
+#define FALSE (!TRUE)
 
-MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
+#define HEAP_SIZE 4096
+#define HEAP_START (header_t *) 5000
+#define HEAP_END (header_t *) (HEAP_START + HEAP_SIZE)
 
-void *allocMemory(MemoryManagerADT const restrict memoryManager, const size_t memoryToAllocate);
+#define EOL_SIZE HEADER_SIZE
+//Seteo el size en par = free
+#define MASK_LAST_BIT(num) (((num) >> 1) << 1)
 
-void freeMemory(MemoryManagerADT const restrict memoryManager, void *memoryToFree);
+//Como los size (real) son par, transforma el valor en par mas cercano para abajo
+//No tiene en cuenta el bit de IsAllocated
+// -2 = 1111 .... 1110
+#define GET_SIZE(ptr) (*ptr & -2)
 
-// TODO: Hay que agregar un status (lo pide la consigna)
-// -> Memoria total ocupada, libre y cualq otra variable necesaria
+//Es impar = allocated?
+#define IS_ALLOCATED(ptr) ((header_t)*(ptr) & 1)
+
+//Hacer el size impar = allocated
+#define SET_ALLOCATED(size) ((size) | 1)
+
+//Check if size is 1 = real size (0) and is allocated(1)
+#define IS_EOL(ptr) ((header_t)*(ptr) == 1)
+
+void freeBlock(header_t * ptr);
+void addBlock(header_t * ptr, uint32_t len);
+header_t * findFree(uint32_t len);
+void addEOL(header_t * ptr);
+void mm_init();
+void * mm_malloc(uint32_t len);
+void mm_free(void * ptr);
+
+
 
 #endif
--- x64barebones/Kernel/memoryManager/memoryManager.c
+++ x64barebones/Kernel/memoryManager/memoryManager.c
@@ -1,53 +1,23 @@
-#include "memoryManager.h"
-#include <stdlib.h>
-
-typedef uint64_t header_t;
-
-#define HEADER_SIZE sizeof(header_t)
-#define TRUE 1
-#define FALSE (!TRUE)
-
-#define HEAP_SIZE 4096
-#define EOL_SIZE HEADER_SIZE
-//Seteo el size en par = free
-#define MASK_LAST_BIT(num) (((num) >> 1) << 1)
-
-//Como los size (real) son par, transforma el valor en par mas cercano
-//No tiene en cuenta el bit de IsAllocated
-// -2 = 1111 .... 1110
-#define GET_SIZE(ptr) ((uint64_t)*(ptr) & -2)
-
-//Es impar = allocated?
-#define IS_ALLOCATED(ptr) ((uint64_t)*(ptr) & 1)
-
-//Hacer el size impar = allocated
-#define SET_ALLOCATED(size) ((size) | 1)
-
-//Check if size is 1 = real size (0) and is allocated(1)
-#define IS_EOL(ptr) ((uint64_t)*(ptr) == 1)
-
-
-
-static void * heapStart = 5000;
-static void * heapEnd = heapStart + HEAP_SIZE;
-
+#include <memoryManager.h>
 
 // TODO: CHEQUEO DE ERRORES
 
 // TODO: HACER EL FREE PARA ATRAS TAMBIEN
-void freeBlock(void * ptr) {
+void freeBlock(header_t * ptr) {
     *ptr = GET_SIZE(ptr);
-    void * next = ptr + *ptr;
+    header_t * next = ptr + *ptr;
 
     if(IS_ALLOCATED(next) == FALSE)
         *ptr = *ptr + *next;
 }
 
 
+
+
 // Allocate in a free block, split if needed
-void addBlock(void * ptr, uint64_t len) {
-    uint64_t newSize = MASK_LAST_BIT(len + 1); // we also round the number (2-Byte align)
-    uint64_t oldSize = GET_SIZE(ptr);
+void addBlock(header_t * ptr, uint32_t len) {
+    uint32_t newSize = MASK_LAST_BIT(len + 1); // we also round the number (2-Byte align)
+    uint32_t oldSize = GET_SIZE(ptr);
 
     if(newSize + HEADER_SIZE >= oldSize)
     	*ptr = SET_ALLOCATED(oldSize);
@@ -57,16 +27,16 @@ void addBlock(void * ptr, uint64_t len) {
     }
 }
 
-void * findFree(uint64_t len) {
-    void * ptr = heapStart;
+header_t * findFree(uint32_t len) {
+    header_t * ptr = HEAP_START;
 
-    while( !IS_EOL(ptr) && (IS_ALLOCATED(ptr) || (*ptr < len) )
+    while( !IS_EOL(ptr) && (IS_ALLOCATED(ptr) || (*ptr < len)) )
         ptr = ptr + GET_SIZE(ptr);
 
 
     if(IS_EOL(ptr)){
     	//Si es el EOL chequeo si entra
-    	if(ptr + HEADER_SIZE + len > heapEnd)
+    	if(ptr + HEADER_SIZE + len > HEAP_END)
     		return NULL;
     	addEOL(ptr + EOL_SIZE + len);
     }
@@ -75,37 +45,38 @@ void * findFree(uint64_t len) {
 }
 
 // Add End Of List block
-void addEOL(void * ptr) {
-    *ptr = (uint64_t) 1;  // size = 0 y allocated = 1
+void addEOL(header_t * ptr) {
+    *ptr = (header_t) 1;  // size = 0 y allocated = 1
 }
 
 
 //TODO Ver si queremos hacer el heap dinamico (setear inicio y size)
 void mm_init() {
-    addEOL(heapStart);
+    addEOL(HEAP_START);
 }
 
 
-void * mm_malloc(uint64_t len) {
+void * mm_malloc(uint32_t len) {
     if(len == 0) 
         return NULL;
 
-    void * out = findFree(len + HEADER_SIZE);
+    header_t * out = findFree(len + HEADER_SIZE);
     addBlock(out, len + HEADER_SIZE);
 
     //User has a pointer after the header
-    return out + HEADER_SIZE;
+    return (void *)(out + HEADER_SIZE);
 }
 
 
 void mm_free(void * ptr) {
-    if(ptr == NULL || ptr < heapStart || ptr >= heapEnd)
+	header_t * cPtr = (header_t *) ptr;
+    if(cPtr == NULL || cPtr < HEAP_START || cPtr >= HEAP_END)
         return;
     
     //Free the header just before the user pointer
-    freeBlock(ptr - HEADER_SIZE);
+    freeBlock(cPtr - HEADER_SIZE);
     //1011 -> 1010 
-    *ptr = MASK_LAST_BIT(*ptr);
+    *cPtr = MASK_LAST_BIT(*cPtr);
 }
 
 
