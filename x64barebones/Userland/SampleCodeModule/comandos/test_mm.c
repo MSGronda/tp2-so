b2356e36e5df766578a5a913b032609943f9fb0a
Manuel Dithurbide <mdithurbide@itba.edu.ar>
2022-10-23 00:53:04 -0300

    feat: mm test seem to work

--- x64barebones/Userland/SampleCodeModule/comandos/test_mm.c
+++ x64barebones/Userland/SampleCodeModule/comandos/test_mm.c
@@ -4,52 +4,65 @@
 #include "../include/stdlib.h"
 #include "../include/syscalls.h"
 
-#define MAX_BLOCKS 128
-
+#define MAX_BLOCKS 50
+#define MAX_MEMORY 30000
 typedef struct MM_rq{
   void *address;
   uint32_t size;
 }mm_rq;
 
-void test_mm(){
-
-  mm_rq mm_rqs[MAX_BLOCKS];
-  uint8_t rq;
-  uint32_t total;
-  uint64_t max_memory = 1024;
-
-  while (1){
-    rq = 0;
-    total = 0;
-
-    // Request as many blocks as we can
-    while(rq < MAX_BLOCKS && total < max_memory){
-      mm_rqs[rq].size = GetUniform(max_memory - total - 1) + 1;
-      mm_rqs[rq].address = malloc(mm_rqs[rq].size);
-
-      if(mm_rqs[rq].address){
-        total += mm_rqs[rq].size;
-        rq++;
-      }
+void* setmem(void* destiation, int32_t c, uint64_t length) {
+    uint8_t chr = (uint8_t)c;
+    char* dst = (char*)destiation;
+    while (length--) {
+        dst[length] = chr;
     }
+    return destiation;
+}
 
-    // Set
-    uint32_t i;
-    for (i = 0; i < rq; i++)
-      if (mm_rqs[i].address)
-        memset(mm_rqs[i].address, i, mm_rqs[i].size);
+void test_mm(){
+    mm_rq mm_rqs[MAX_BLOCKS];
+    uint8_t rq;
+    uint32_t total;
+    uint64_t max_memory;
 
-    // Check
-    for (i = 0; i < rq; i++)
-      if (mm_rqs[i].address)
-        if(!memcheck(mm_rqs[i].address, i, mm_rqs[i].size)){
-          puts("test_mm ERROR\n");
-          return -1;
+    max_memory = MAX_MEMORY;
+
+    while (1) {
+        rq = 0;
+        total = 0;
+
+        puts("TESTING MEMORY MANAGER - PRESS ESC TO FINISH TEST");
+
+        // Request as many blocks as we can
+        while (rq < MAX_BLOCKS && total < max_memory) {
+            mm_rqs[rq].size = GetUniform(max_memory - total - 1) + 1;
+            mm_rqs[rq].address = malloc(mm_rqs[rq].size);
+
+            if (mm_rqs[rq].address) {
+                total += mm_rqs[rq].size;
+                rq++;
+            }
         }
 
-    // Free
-    for (i = 0; i < rq; i++)
-      if (mm_rqs[i].address)
-        free(mm_rqs[i].address);
-  } 
+        // Set
+        uint32_t i;
+        for (i = 0; i < rq; i++)
+            if (mm_rqs[i].address)
+                setmem(mm_rqs[i].address, i, mm_rqs[i].size);
+
+        // Check
+        for (i = 0; i < rq; i++)
+            if (mm_rqs[i].address)
+                if (!memcheck(mm_rqs[i].address, i, mm_rqs[i].size)) {
+                    puts("test_mm ERROR");
+                    return;
+                }
+
+        // Free
+        for (i = 0; i < rq; i++)
+            if (mm_rqs[i].address)
+                free(mm_rqs[i].address);
+        puts("Test passed!");   
+    }
 }
