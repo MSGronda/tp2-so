16929cfa01310ee702e782dfccb996c1d7e17e54
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-11-03 17:52:52 -0300

    refactor: updated help

--- x64barebones/Userland/SampleCodeModule/comandos/help.c
+++ x64barebones/Userland/SampleCodeModule/comandos/help.c
@@ -1,13 +1,14 @@
 #include "../include/comandos.h"
 
+#define CANT_PAGS 6
+
 void page0()
 {
     puts("= = = = = = = = = MULTITASKING = = = = = = = = ");
-    puts("   - Para ejecucion simultanea comando1 | comando2\n");
     puts("   - Con tecla - F1 - se pausa/reanuda ejecucion de modo normal");
     puts("   - Con tecla - ESC - regresa a la terminal unica");
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n",13);
-    puts("Pag 1/4 - proxima apretando \".\" - previa apretando \",\"");
+    puts("Pag 1/6 - proxima apretando \".\" - previa apretando \",\"");
 }
 
 void page1()
@@ -17,42 +18,59 @@ void page1()
     puts("   - primos:\n    imprime los numeros primos\n    se corta ejecucion con la tecla ESC\n");
     puts("   - time:\n    imprime el dia y hora del sistema\n");
     puts("   - printargs:\n    imprime los argumentos pasados por linea de comandos\n");
-    puts("   - pipe:\n    imprime la lista de pipes con sus propiedades\n");
     puts("   - filter:\n    filtra las vocales del input\n");
     puts("   - wc:\n    cuenta la cantidad de lineas del input\n");
-    puts("Pag 2/4 - proxima apretando \".\" - previa apretando \",\"");
+    puts("Pag 2/6 - proxima apretando \".\" - previa apretando \",\"");
 
     return;
 }
 
-void page2() {
+void page2()
+{
     puts("= = = = = = = = COMANDOS = = = = = = = = ");
-    puts("   - sem:\n    imprime la lista de semaforos con sus propiedades\n");
-    puts("   - ps:\n    imprime la lista de procesos con sus propiedades\n");
     puts("   - kill:\n    mata a un proceso dado su ID\n");
     puts("   - block:\n    cambia el estado de un proceso entre bloqueado y listo dado su ID\n");
     puts("   - nice:\n    cambia la prioridad de un proceso dado su ID y su nueva prioridad\n");
-    puts("   - cat:\n    imprime el stdin tal como lo recibe\n");
     puts("   - loop:\n    imprime su ID con un saludo cada una determinada cantidad de segundos\n");
-    puts("Pag 3/4 - proxima apretando \".\" - previa apretando \",\"");
+    puts("   - cat:\n    imprime el stdin tal como lo recibe\n");
+    puts("   - phylo:\n    corre el problema de los filosofos. \n");
+    puts("   pulse 'a' o 'r' para agregar o quitar filosofos respectivamente.\n");
+    puts("    pulse 'q' para terminar de manera controlada. \n");
+    puts("Pag 3/6 - proxima apretando \".\" - previa apretando \",\"");
 
     return;
 }
 
 void page3() {
-    puts("= = = = = = = = COMANDOS = = = = = = = = ");
-    puts("   - div-error:\n    ejecuta una division por 0 para testear la excepcion\n");
-    puts("   - opcode-error:\n    ejecuta una operacion invalida para testear la excepcion\n");
-    puts("= = = = = = = = TESTS = = = = = = = = ");
-    puts("   - test-sem:\n    corre un testeo de semaforos\n");
-    puts("   - test-mm:\n    corre un testeo del memory manager\n");
-    puts("   - test-process:\n    corre un testeo de procesos\n");
-    puts("   - test-prio:\n    corre un testeo de prioridades\n");
-    puts("Pag 4/4 - proxima apretando \".\" - previa apretando \",\"");
+    puts("= = = = = = = = STATUS = = = = = = = = ");
+    puts("   - sem:\n    imprime la lista de semaforos con sus propiedades\n");
+    puts("   - ps:\n    imprime la lista de procesos con sus propiedades\n");
+    puts("   - pipe:\n    imprime la lista de pipes con sus propiedades\n");
+    puts("   - mmstatus:\n    imprime el status del memory manager\n");
+    puts("Pag 4/6 - proxima apretando \".\" - previa apretando \",\"");
 
     return;
 }
 
+void page4() {
+    puts("= = = = = = = = TESTS = = = = = = = = ");
+    puts("   - test-sem:\n    corre un testeo de semaforos (con semaforos)\n");
+    puts("   - test-no-sem:\n    corre un testeo de semaforos (sin semaforos)\n");
+    puts("   - test-mm:\n    corre un testeo del memory manager\n");
+    puts("   - test-process:\n    corre un testeo de procesos\n");
+    puts("   - test-prio:\n    corre un testeo de prioridades\n");
+    puts("   - test-pipe:\n    corre un testeo de pipes\n");
+    puts("Pag 5/6 - proxima apretando \".\" - previa apretando \",\"");
+
+    return;
+}
+
+void page5() {
+    puts("= = = = = = = = ERRORES = = = = = = = = ");
+    puts("   - div-error:\n    ejecuta una division por 0 para testear la excepcion\n");
+    puts("   - opcode-error:\n    ejecuta una operacion invalida para testear la excepcion\n");
+    puts("Pag 6/6 - proxima apretando \".\" - previa apretando \",\"");
+}
 
 void switchPage(int page)
 {
@@ -74,6 +92,14 @@ void switchPage(int page)
             page3();
             break;
 
+        case 4:
+            page4();
+            break;
+        
+        case 5:
+            page5();
+            break;
+
         default:
             break;
     }
@@ -91,13 +117,13 @@ void help()
 
         if(strContainsChar(buffer, '.')>=0) {                    // ## REMPLAZAR ##
             currPage++;
-            currPage = currPage%4;
+            currPage = currPage % CANT_PAGS;
             switchPage(currPage);
         } else if(strContainsChar(buffer, ',')>=0){                    // ## REMPLAZAR ##
             currPage--;
 
             if(currPage < 0)
-                currPage += 4;
+                currPage += CANT_PAGS;
 
             switchPage(currPage);
         }
